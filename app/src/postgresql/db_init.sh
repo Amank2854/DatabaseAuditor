4f060de2a8e4d80a4e7193fd9ca03e04db0be2a3
Ojassvi Kumar <kumarojassvi@gmail.com>
2023-04-09 01:55:20 +0530

    Updated postgresql initialisation script

--- app/src/postgresql/db_init.sh
+++ app/src/postgresql/db_init.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/bash
 
-sudo -u postgres -H -- psql -d postgres -U postgres -c "\i app/src/postgresql/db_init.sql"
+sudo -u postgres -H -- psql -d postgres -U postgres -c "\i /home/phyo/abaw5/DatabaseAuditor/app/src/postgresql/data/db_init.sql"
