518761d8669e8d6a9c94258ddad3c2fe387804c3
OptimalKnight <kumarojassvi@gmail.com>
2023-04-23 16:58:22 +0530

    Code refactoring

--- app/src/main/java/databaseauditor/Database/Neo4j.java
+++ app/src/main/java/databaseauditor/Database/Neo4j.java
@@ -21,8 +21,13 @@ public class Neo4j implements Database {
 
     @Override
     public boolean connect(String url, String username, String password) {
-        driver = GraphDatabase.driver(url, AuthTokens.basic(user, password));
-        return true;
+        try {
+            driver = GraphDatabase.driver(url, AuthTokens.basic(user, password));
+            return true;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
     }
 
     @Override
@@ -32,8 +37,6 @@ public class Neo4j implements Database {
 
     public <T> int insertOne(T obj) {
         Field[] fields = obj.getClass().getDeclaredFields();
-
-        String label = obj.getClass().getName().split("\\.")[obj.getClass().getName().split("\\.").length - 1];
         List<String> propertyKey = new ArrayList<>();
         List<Object> propertyValue = new ArrayList<>();
 
@@ -42,38 +45,36 @@ public class Neo4j implements Database {
                 propertyKey.add(field.getName().toString());
                 propertyValue.add(field.get(obj).toString());
             } catch (IllegalArgumentException e) {
-                System.out.println("Error: " + e.getMessage());
+                System.out.println(e.getMessage());
                 return -1;
             } catch (IllegalAccessException e) {
-                System.out.println("Error: " + e.getMessage());
+                System.out.println(e.getMessage());
                 return -1;
             }
         }
 
-        String query = "CREATE (n:" + label + " {";
+        String query = "CREATE (n:" + obj.getClass().getName().split("\\.")[obj.getClass().getName().split("\\.").length - 1] + " {";
         for (int i = 0; i < propertyKey.size(); i++) {
             query = query + propertyKey.get(i) + ": \"" + propertyValue.get(i).toString() + "\"";
             if (i != propertyKey.size() - 1) {
                 query = query + ", ";
             }
         }
+
         query = query + "});";
-
-        System.out.println(query);
-
         try (Session session = driver.session()) {
             String finalQuery = query;
             session.writeTransaction(tx -> tx.run(finalQuery));
+            return 1;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return -1;
         }
-
-        return 1;
     }
 
     @Override
     public <T> int updateMany(T obj, List<List<String>> params) {
         Field[] fields = obj.getClass().getDeclaredFields();
-
-        String label = obj.getClass().getName().split("\\.")[obj.getClass().getName().split("\\.").length - 1];
         List<String> fieldNames = new ArrayList<String>();
         String updates = "", conditions = "";
 
@@ -83,10 +84,10 @@ public class Neo4j implements Database {
                 updates = updates + "n." + field.getName().toString() + " = ";
                 updates = updates + "\"" + field.get(obj).toString() + "\", ";
             } catch (IllegalArgumentException e) {
-                System.out.println("Error: " + e.getMessage());
+                System.out.println(e.getMessage());
                 return -1;
             } catch (IllegalAccessException e) {
-                System.out.println("Error: " + e.getMessage());
+                System.out.println(e.getMessage());
                 return -1;
             }
         }
@@ -96,30 +97,28 @@ public class Neo4j implements Database {
                 conditions = conditions + " " + param.get(0) + " : ";
                 conditions = conditions + "\"" + param.get(1) + "\", ";
             } else {
-                System.out.println("ERROR: Invalid paramater: " + param.get(0));
+                System.out.println("INVALID PARAMETER: " + param.get(0));
                 return -1;
             }
         }
 
         updates = updates.substring(0, updates.length() - 2);
         conditions = conditions.substring(0, conditions.length() - 2);
-        String query = "MATCH (n:" + label + " {" + conditions + "}) " + " set " + updates + ";";
-
-        System.out.println(query);
-
+        String query = "MATCH (n:" + obj.getClass().getName().split("\\.")[obj.getClass().getName().split("\\.").length - 1] + " {" + conditions + "}) " + " set " + updates + ";";
         try (Session session = driver.session()) {
             String finalQuery = query;
             session.writeTransaction(tx -> tx.run(finalQuery));
+            // TODO: return number of updates
+            return 1;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return -1;
         }
-
-        return 1;
     }
 
     @Override
     public <T> int deleteMany(T obj, List<List<String>> params) {
         Field[] fields = obj.getClass().getDeclaredFields();
-
-        String label = obj.getClass().getName().split("\\.")[obj.getClass().getName().split("\\.").length - 1];
         List<String> fieldNames = new ArrayList<String>();
         String conditions = "";
 
@@ -127,7 +126,7 @@ public class Neo4j implements Database {
             try {
                 fieldNames.add(field.getName().toString());
             } catch (IllegalArgumentException e) {
-                System.out.println("Error: " + e.getMessage());
+                System.out.println(e.getMessage());
                 return -1;
             }
         }
@@ -137,30 +136,25 @@ public class Neo4j implements Database {
                 conditions = conditions + " " + param.get(0) + " : ";
                 conditions = conditions + "\"" + param.get(1) + "\", ";
             } else {
-                System.out.println("ERROR: Invalid paramater: " + param.get(0));
+                System.out.println("INVALID PARAMETER: " + param.get(0));
                 return -1;
             }
         }
 
         conditions = conditions.substring(0, conditions.length() - 2);
-        String query = "MATCH (n:" + label + " {" + conditions + "}) " + " delete n;";
-
-        System.out.println(query);
-
+        String query = "MATCH (n:" + obj.getClass().getName().split("\\.")[obj.getClass().getName().split("\\.").length - 1] + " {" + conditions + "}) " + " delete n;";
         try (Session session = driver.session()) {
             String finalQuery = query;
             session.writeTransaction(tx -> tx.run(finalQuery));
+            // TODO: return number of deletes
+            return 1;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return -1;
         }
-
-        return 1;
     }
 
-<<<<<<< HEAD:app/src/main/java/databaseauditor/Database/Neo4j.java
     public <T> int select(T obj, List<List<String>> params, List<String> reqCols){
-        
-=======
-    public <T> int select(T obj, List<List<String>> params, List<String> reqCols) {
->>>>>>> 7594f1eb5176f2806d51ac0884ec6a3c1f07f2e6:app/src/main/java/databaseauditor/Neo4j.java
         return 1;
     }
 }
